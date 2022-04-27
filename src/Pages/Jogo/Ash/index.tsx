36a1a9a809d324946a5cd9e9a993310e1bf7cbdc
Marcel S Fonseca <marcelsfonseca@gmail.com>
2022-04-27 00:23:31 -0300

    Tentativa 2 Movimentação

--- src/Pages/Jogo/Ash/index.tsx
+++ src/Pages/Jogo/Ash/index.tsx
@@ -2,40 +2,40 @@ import ash from "assets/ash.svg";
 import style from "./Ash.module.scss"
 
 function Ash() {
+
     var x_axis = 0;
     var y_axis = 0;
+    const movePersonagem = document.getElementsByClassName("personagem");
 
-    document.onkeydown = check_key;
-
-    function check_key(e: any | null) {
+    document.body.onkeydown = (e: any | null) => {
         e = e || window.Event;
-        if (e.keyCode == '87') {
-            movAsh("up");
-            console.log(KeyboardEvent.DOM_KEY_LOCATION_STANDARD)
-        } else if (e.keyCode == '83') {
-            movAsh("down");
-        } else if (e.keyCode == '65') {
-            movAsh("left");
-        } else if (e.keyCode == '68') {
-            movAsh("right");
+        if (e.key == "w") {
+            console.log("Teste W")
+        } else if (e.key == "s") {
+            console.log("Teste s")
+        } else if (e.key == "a") {
+            console.log("Teste a")
+        } else if (e.key == "d") {
+            console.log("Teste d")
         }
     }
 
     function movAsh(a: string) {
         if (a == 'right') {
             x_axis = x_axis + 10;
-            $(".personagem").css({ "transform": "translate(" + x_axis + "px," + y_axis + "px)" });
+            $("personagem").css ({ "transform": "translate(" + x_axis + "px," + y_axis + "px)" });
         } else if (a == 'left') {
             x_axis = x_axis - 10;
-            $(".personagem").css({ "transform": "translate(" + x_axis + "px," + y_axis + "px)" });
+            $("personagem").css({ "transform": "translate(" + x_axis + "px," + y_axis + "px)" });
         } else if (a == 'up') {
             y_axis = y_axis - 10;
-            $(".personagem").css({ "transform": "translate(" + x_axis + "px," + y_axis + "px)" });
+            $("personagem").css({ "transform": "translate(" + x_axis + "px," + y_axis + "px)" });
         } else if (a == 'down') {
             y_axis = y_axis + 10;
-            $(".personagem").css({ "transform": "translate(" + x_axis + "px," + y_axis + "px)" });
+            $("personagem").css({ "transform": "translate(" + x_axis + "px," + y_axis + "px)" });
         }
     }
+
     return (
         <div className={style.personagem}><img src={ash} alt="Personagem" /></div>
     );
